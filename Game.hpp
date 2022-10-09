989ba4651260a233e827e1367342b3af1c81ac04
Gustavo Silvera <grs5382@gmail.com>
2022-10-09 11:39:30 -0400

    formatting with webkit

--- Game.hpp
+++ Game.hpp
@@ -2,86 +2,85 @@
 
 #include <glm/glm.hpp>
 
-#include <string>
 #include <list>
 #include <random>
+#include <string>
 
 struct Connection;
 
-//Game state, separate from rendering.
+// Game state, separate from rendering.
 
-//Currently set up for a "client sends controls" / "server sends whole state" situation.
+// Currently set up for a "client sends controls" / "server sends whole state" situation.
 
 enum class Message : uint8_t {
-	C2S_Controls = 1, //Greg!
-	S2C_State = 's',
-	//...
+    C2S_Controls = 1, // Greg!
+    S2C_State = 's',
+    //...
 };
 
-//used to represent a control input:
+// used to represent a control input:
 struct Button {
-	uint8_t downs = 0; //times the button has been pressed
-	bool pressed = false; //is the button pressed now
+    uint8_t downs = 0; // times the button has been pressed
+    bool pressed = false; // is the button pressed now
 };
 
-//state of one player in the game:
+// state of one player in the game:
 struct Player {
-	//player inputs (sent from client):
-	struct Controls {
-		Button left, right, up, down, jump;
+    // player inputs (sent from client):
+    struct Controls {
+        Button left, right, up, down, jump;
 
-		void send_controls_message(Connection *connection) const;
+        void send_controls_message(Connection* connection) const;
 
-		//returns 'false' if no message or not a controls message,
-		//returns 'true' if read a controls message,
-		//throws on malformed controls message
-		bool recv_controls_message(Connection *connection);
-	} controls;
+        // returns 'false' if no message or not a controls message,
+        // returns 'true' if read a controls message,
+        // throws on malformed controls message
+        bool recv_controls_message(Connection* connection);
+    } controls;
 
-	//player state (sent from server):
-	glm::vec2 position = glm::vec2(0.0f, 0.0f);
-	glm::vec2 velocity = glm::vec2(0.0f, 0.0f);
+    // player state (sent from server):
+    glm::vec2 position = glm::vec2(0.0f, 0.0f);
+    glm::vec2 velocity = glm::vec2(0.0f, 0.0f);
 
-	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
-	std::string name = "";
+    glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+    std::string name = "";
 };
 
 struct Game {
-	std::list< Player > players; //(using list so they can have stable addresses)
-	Player *spawn_player(); //add player the end of the players list (may also, e.g., play some spawn anim)
-	void remove_player(Player *); //remove player from game (may also, e.g., play some despawn anim)
+    std::list<Player> players; //(using list so they can have stable addresses)
+    Player* spawn_player(); // add player the end of the players list (may also, e.g., play some spawn anim)
+    void remove_player(Player*); // remove player from game (may also, e.g., play some despawn anim)
 
-	std::mt19937 mt; //used for spawning players
-	uint32_t next_player_number = 1; //used for naming players
+    std::mt19937 mt; // used for spawning players
+    uint32_t next_player_number = 1; // used for naming players
 
-	Game();
+    Game();
 
-	//state update function:
-	void update(float elapsed);
+    // state update function:
+    void update(float elapsed);
 
-	//constants:
-	//the update rate on the server:
-	inline static constexpr float Tick = 1.0f / 30.0f;
+    // constants:
+    // the update rate on the server:
+    inline static constexpr float Tick = 1.0f / 30.0f;
 
-	//arena size:
-	inline static constexpr glm::vec2 ArenaMin = glm::vec2(-0.75f, -1.0f);
-	inline static constexpr glm::vec2 ArenaMax = glm::vec2( 0.75f,  1.0f);
+    // arena size:
+    inline static constexpr glm::vec2 ArenaMin = glm::vec2(-0.75f, -1.0f);
+    inline static constexpr glm::vec2 ArenaMax = glm::vec2(0.75f, 1.0f);
 
-	//player constants:
-	inline static constexpr float PlayerRadius = 0.06f;
-	inline static constexpr float PlayerSpeed = 2.0f;
-	inline static constexpr float PlayerAccelHalflife = 0.25f;
-	
+    // player constants:
+    inline static constexpr float PlayerRadius = 0.06f;
+    inline static constexpr float PlayerSpeed = 2.0f;
+    inline static constexpr float PlayerAccelHalflife = 0.25f;
 
-	//---- communication helpers ----
+    //---- communication helpers ----
 
-	//used by client:
-	//set game state from data in connection buffer
-	// (return true if data was read)
-	bool recv_state_message(Connection *connection);
+    // used by client:
+    // set game state from data in connection buffer
+    //  (return true if data was read)
+    bool recv_state_message(Connection* connection);
 
-	//used by server:
-	//send game state.
-	//  Will move "connection_player" to the front of the front of the sent list.
-	void send_state_message(Connection *connection, Player *connection_player = nullptr) const;
+    // used by server:
+    // send game state.
+    //   Will move "connection_player" to the front of the front of the sent list.
+    void send_state_message(Connection* connection, Player* connection_player = nullptr) const;
 };
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -5,30 +5,29 @@
 
 #include <glm/glm.hpp>
 
-#include <vector>
 #include <deque>
+#include <vector>
 
 struct PlayMode : Mode {
-	PlayMode(Client &client);
-	virtual ~PlayMode();
+    PlayMode(Client& client);
+    virtual ~PlayMode();
 
-	//functions called by main loop:
-	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
-	virtual void update(float elapsed) override;
-	virtual void draw(glm::uvec2 const &drawable_size) override;
+    // functions called by main loop:
+    virtual bool handle_event(SDL_Event const&, glm::uvec2 const& window_size) override;
+    virtual void update(float elapsed) override;
+    virtual void draw(glm::uvec2 const& drawable_size) override;
 
-	//----- game state -----
+    //----- game state -----
 
-	//input tracking for local player:
-	Player::Controls controls;
+    // input tracking for local player:
+    Player::Controls controls;
 
-	//latest game state (from server):
-	Game game;
+    // latest game state (from server):
+    Game game;
 
-	//last message from server:
-	std::string server_message;
+    // last message from server:
+    std::string server_message;
 
-	//connection to server:
-	Client &client;
-
+    // connection to server:
+    Client& client;
 };
